b5eebd2df301a7d0e601dec2bbc70987a97b5839
rfka <kaminski.lista@gmail.com>
2021-10-05 12:33:37 -0300

    Alguma correçõezinhas
    *****

--- front/static/index.js
+++ front/static/index.js
@@ -3,34 +3,39 @@ const urlBD = 'http://localhost:3000/favMovies';
 const listarMovies = document.getElementById('lista');
 
 let editando = false;
-let idEditando = null;
+let idEditando = 0;
 
 const getMovies = async () => {
     const response = await fetch(urlBD);
 
     const data = await response.json();
-    console.log(data);
 
     data.map((movie) => {
         listarMovies.insertAdjacentHTML('beforeend', `
         <div class="">
         <div class="card">
         <img src="${movie.urlImagem}" class="card-img-top" alt="${movie.nome}">
-        <div class="card-body">
-            <span class="card-title" style="font-size:30px">${movie.nome}</span>
-        </div>
-        <div class="card-body1">
-            <div>
-                <span><b>${movie.nome}</b></span><br>
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-                Assistido
-            </label>
+        <div class="card-titles">
+            <div class="card-body">
+                <span class="card-title" style="font-size:30px">${movie.nome}</span>
             </div>
-            <div class="nota">
-                <span style="font-size:25px; color: gray">${movie.nota}</span><br>
+            <div class="card-body1">
+                <div>
+                    <span><b></b></span><br>
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Assistido
+                </label>
+                </div>
+                <div class="nota">
+                    <span style="font-size:25px; color: gray">${movie.nota}</span><br>
+                </div>
+                <div>
+                    <button type="button" onclick="putMovie(${movie.id})">Editar</button>
+                    <button type="button" onclick="delMovie(${movie.id})">Excluir</button>
+                </div>
             </div>
-        </div>
+            </div>
         </div>
     </div>
         `)
@@ -40,13 +45,13 @@ const getMovies = async () => {
 
 getMovies();
 
-const submitForm = async (e) => {
-    e.preventDefault(); 
+const submitForm = async (envio) => {
+    envio.preventDefault(); 
 
-    let nome = document.getElementById('txt-nome');
-    let imagem = document.getElementById('txt-imagem');
-    let genero = document.getElementById('txt-genero');
-    let nota = document.getElementById('txt-nota');
+    let nome = document.getElementById('nome');
+    let imagem = document.getElementById('imagem');
+    let genero = document.getElementById('genero');
+    let nota = document.getElementById('nota');
 
     const movie = {
         nome: nome.value,
@@ -92,35 +97,35 @@ const submitForm = async (e) => {
     nota.value = '';
 
     lista.innerHTML = '';
-}
+};
 
 const getMoviesById = async (id) => {
     const response = await fetch(`${urlBD}/${id}`);
     return movie = response.json();
-}
+};
 
 const putMovie = async (id) => {
-    editando = true;
+    editando = false;
     idEditando = id;
 
     const movie = await getMovieById(id);
 
-    let nomeNovo = document.getElementById(txt-nome);
-    let imagemNovo = document.getElementById(txt-nmagem);
-    let generoNovo = document.getElementById(txt-nenero);
-    let notaNovo = document.getElementById(txt-nota);
+    let nomeNovo = document.getElementById(nome);
+    let imagemNovo = document.getElementById(imagem);
+    let generoNovo = document.getElementById(genero);
+    let notaNovo = document.getElementById(nota);
 
     nomeNovo.value = movie.nome;
     imagemNovo.value = movie.imagem;
     generoNovo.value = movie.genero;
     notaNovo.value = movie.nota;
 
-}
+};
 
 const delMovie = async (id) => {
     const request = new Request(`${urlBD}/${id}` , {
         method: 'DELETE',
-    })
+    });
 
     const response = await fetch(request);
     const data = await response.json();
